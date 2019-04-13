3c7fb361a8f10feb64da0fe391bd1a8094b5d21b
jamaly87 <49349946+jamaly87@users.noreply.github.com>
2019-04-13 12:07:43 -0400

    Update and rename ex9.py to 9.4.py

--- 9.4.py
+++ 9.4.py
@@ -1,3 +1,5 @@
+#9.4 Write a program to read through the mbox-short.txt and figure out who has sent the greatest number of mail messages. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
+
 #input the file name if we press enter we'll immediately assume that the file is mbox-short
 filename=input('Please Enter File Name: ')
 if len(filename)<1:filename='mbox-short.txt'
